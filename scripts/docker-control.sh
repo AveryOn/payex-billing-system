2c603f7e1b4501f028f4a11a132692c19e214108
AveryOn <lnkonion2@gmail.com>
2026-06-20 20:07:52 +0400

    add docker-control script

---
+++ scripts/docker-control.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -eu
+
+ENVIRONMENT="${1:-}"
+ACTION="${2:-}"
+
+case "$ENVIRONMENT" in
+  dev)
+    ENV_FILE=".env.development"
+    COMPOSE_FILE="docker-compose.development.yml"
+    ;;
+  prod)
+    ENV_FILE=".env.production"
+    COMPOSE_FILE="docker-compose.production.yml"
+    ;;
+  *)
+    echo "Usage: $0 <dev|prod> <start|stop>" >&2
+    exit 1
+    ;;
+esac
+
+case "$ACTION" in
+  start|stop)
+    ;;
+  *)
+    echo "Usage: $0 <dev|prod> <start|stop>" >&2
+    exit 1
+    ;;
+esac
+
+docker compose \
+  --env-file "$ENV_FILE" \
+  -f "$COMPOSE_FILE" \
+  "$ACTION"
